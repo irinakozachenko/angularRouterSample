25a9acc1e95ede49b47c75a84af13fc638734d04
Irina Kozachenko <irina.kozachenko@dsr-corporation.com>
2021-02-09 15:17:11 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      CrisisListComponent,
+      HeroesListComponent,
+      PageNotFoundComponent
+   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+      { path: 'crisis-list', component: CrisisListComponent },
+      { path: 'heroes-list', component: HeroesListComponent },
+      { path: '', redirectTo: '/heroes-list', pathMatch: "full" },
+      { path: '**', component: PageNotFoundComponent }
+    ]),
     AppRoutingModule
   ],
   providers: [],
